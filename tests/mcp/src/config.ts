1fdf1eb48d926988264ef26753cc9943d2b35d2a
Reda AGAOUA <reda.agaoua@gmail.com>
2026-03-26 15:40:40 +0100

    tests/keycloak: sanitize realm config
    Had useless elements from other project

--- tests/mcp/src/config.ts
+++ tests/mcp/src/config.ts
@@ -15,10 +15,10 @@ export const CONFIG: Config = {
   host: process.env.HOST || "localhost",
   port: Number(process.env.PORT) || 3000,
   auth: {
-    issuer: "http://localhost:8080/realms/myrealm",
+    issuer: "http://localhost:8080/realms/a3s-realm",
     // jwksUri:
-    //   "http://localhost:8080/realms/myrealm/protocol/openid-connect/certs",
-    clientId: "chatbot-app",
+    //   "http://localhost:8080/realms/a3s-realm/protocol/openid-connect/certs",
+    clientId: "a3s-test-client",
     allowedJwtAlgs: ["RS256"],
   },
 };
